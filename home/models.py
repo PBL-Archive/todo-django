b2d303cf5e9bd0459827db07499be5102e6185a3
baijudodhia <51258945+baijudodhia@users.noreply.github.com>
2020-07-10 01:43:14 +0530

    Todo model created

--- home/models.py
+++ home/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Todo(models.Model):
+    todo_id = models.AutoField(primary_key=True)
+    todo = models.TextField(max_length=100)
+    
+    def __str__(self):
+        return self.todo
