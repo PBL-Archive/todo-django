908994cfd4c65ffac1ca1fa6c279bdc93e053da5
baijudodhia <51258945+baijudodhia@users.noreply.github.com>
2020-07-10 06:30:27 +0530

    Add, Edit, Remove and Check functionality done + Todo model updated

--- home/models.py
+++ home/models.py
@@ -5,6 +5,11 @@ from django.db import models
 class Todo(models.Model):
     todo_id = models.AutoField(primary_key=True)
     todo = models.TextField(max_length=100)
-    
+    checked = models.CharField(max_length=20, default="not_checked")
+    todotext = models.CharField(max_length=20, default="cannot_edit")
+    edit = models.CharField(max_length=20, default="edit")
+    remove = models.CharField(max_length=20, default="remove")
+    add = models.CharField(max_length=20, default="add")
+
     def __str__(self):
         return self.todo
--- home/views.py
+++ home/views.py
@@ -5,23 +5,58 @@ from home.models import Todo
 
 
 def home(request):
-    if request.method == "POST":
-        checked_btn = request.POST.get('checked')
-        edit_btn = request.POST.get('edit')
-        remove_btn = request.POST.get('remove')
-        add_btn = request.POST.get('add')
-        print("Before", checked_btn)
-        if checked_btn == "not_checked":
-            checked_btn = "checked"
-        else:
-            checked_btn = "not_checked"
-        print("After", checked_btn)
-        print(checked_btn, edit_btn, remove_btn, add_btn)
-        context = {
-            'checked_btn': checked_btn,
-            'edit_btn': edit_btn,
-            'remove_btn': remove_btn,
-            'add_btn': add_btn,
-        }
-        return render(request, 'todo.html', context)
-    return render(request, 'todo.html')
+    todos = Todo.objects.all().values()
+    context = {
+        'todos': todos,
+    }
+    if 'add' in request.POST:
+        ts_todo_add = request.POST.get('new_todo')
+        if ts_todo_add is not "":
+            print(ts_todo_add)
+            t = Todo(todo=ts_todo_add)
+            t.save()
+    if not 'add' in request.POST:
+        print("NOT")
+        todoid = request.POST.get('todoid')
+        todo_specific = Todo.objects.filter(todo_id=todoid).values()
+        ts_id = ""
+        ts_todo = ""
+        ts_checked = ""
+        ts_todotext = ""
+        ts_edit = ""
+        ts_remove = ""
+        ts_add = ""
+        for t in todo_specific:
+            ts_id = t['todo_id']
+            ts_todo = t['todo']
+            ts_checked = t['checked']
+            ts_todotext = t['todotext']
+            ts_edit = t['edit']
+            ts_remove = t['remove']
+            ts_add = t['add']
+        if 'checked' in request.POST:
+            if ts_checked == "checked":
+                ts_checked = "not_checked"
+                ts_edit = "edit"
+            else:
+                ts_checked = "checked"
+                ts_todotext = "cannot_edit"
+                ts_edit = "not_edit"
+        if 'todotext' in request.POST:
+            ts_todo_temp = request.POST.get('todotext')
+            if ts_todo_temp != "":
+                ts_todo = ts_todo_temp
+                ts_edit = "edit"
+                ts_todotext = "cannot_edit"
+        if 'edit' in request.POST:
+            if ts_edit == "edit":
+                ts_edit = "active_edit"
+                ts_todotext = "can_edit"
+            elif ts_edit == "active_edit":
+                ts_edit = "edit"
+                ts_todotext = "cannot_edit"
+        if 'remove' in request.POST:
+            Todo.objects.filter(todo_id=todoid).delete()
+        Todo.objects.filter(todo_id=todoid).update(
+            checked=ts_checked, todo=ts_todo, todotext=ts_todotext, edit=ts_edit)
+    return render(request, 'todo.html', context)
